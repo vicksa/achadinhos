833be8b1676890362853491ee6e0bc802d640a02
Vick <75048284+vicksa@users.noreply.github.com>
2026-08-31 00:33:13 -0300

    feat: criar pacote affiliate

---
+++ backend/affiliate/__init__.py
@@ -0,0 +1,5 @@
+"""Affiliate Engine do Achadinhos."""
+
+from affiliate.engine import AffiliateResult, enrich_affiliate_data, identify_marketplace
+
+__all__ = ["AffiliateResult", "enrich_affiliate_data", "identify_marketplace"]
